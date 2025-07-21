9c73ae03d00764d6b5f5f336e3a2ba019096beee
Deepukumar93 <deepu95760@gmail.com>
2025-07-22 01:54:36 +0530

    update

---
+++ Largest-Number.py
@@ -0,0 +1,12 @@
+x=int(input("Enter the number"))
+y=int(input("Enter the numbetr"))
+z=int(input("Enter the number"))
+
+if (x > y and x > z):
+   print("x is largest" )
+if(y>x or y>z):
+        print("y is largest") 
+if(z>x and z>y):
+    print("z is largest")
+  
+   
